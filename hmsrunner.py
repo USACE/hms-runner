9828eba10ab03c0a796048b2d408010e77d8f28c
Will Lehman <william.p.lehman@usace.army.mil>
2022-05-25 23:56:21 +0000

    what should be a functional payload and run is not working possibly due to quotes

--- hmsrunner.py
+++ hmsrunner.py
@@ -2,7 +2,17 @@ from hms.model.JythonHms import *
 import sys
 
 print("attempt to run hms")
-print(sys.argv)
-OpenProject("tenk", "/tenk/")
-Compute("Jan 96 storm")
+f = open("/workspaces/hms-runner/example_data/payload.yml", "r")
+f.readline()
+f.readline()
+f.readline()
+modelline = f.readline()
+modelparts = modelline.split(': ')
+modelName = modelparts[1]
+print("model name is " + modelName)
+f.readline()
+path = f.readline().split(': ')[1]
+project = f.readline().split(': ')[1].split(".")[0]
+OpenProject(project, path)
+Compute(modelName)
 print("compute complete")

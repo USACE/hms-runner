64debedf94ad50463b4436b71f24dd17d17e906f
Will Lehman <william.p.lehman@usace.army.mil>
2022-08-10 17:19:34 -0400

    updated code to current wat-java-sdk 0.0.2

--- src/main/java/usace/wat/plugin/hmsrunner/hmsrunner.java
+++ src/main/java/usace/wat/plugin/hmsrunner/hmsrunner.java
@@ -2,6 +2,11 @@ package usace.wat.plugin.hmsrunner;
 
 import usace.wat.plugin.*;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import hms.model.Project;
 import hms.Hms;
 
@@ -22,28 +27,34 @@ public class hmsrunner  {
         }
         //first arg should be a modelpayload check to see it is
         String filepath = args[0].split("=")[1];
+        //load payload. 
         ModelPayload mp = Utilities.LoadPayload(filepath);
-        //copy payload to local or read it from S3.     
-        /*
         //copy the model to local if not local
         //hard coded outputdestination is fine in a container
-        String modelOutputDestination = "/workspaces/hms-runner/run/model/"+payload.ModelName()+"/";
+        String modelOutputDestination = "/workspaces/hms-runner/run/model/"+mp.getModel().getName()+"/";
         //download the payload to list all input files
-        for (LinkedDataDescription input : payload.Inputs()) {
-            loader.DownloadFromS3(input.getResourceInfo().getAuthority(), input.getResourceInfo().getFragment(),modelOutputDestination + input.getName());
-        }      
+        Utilities.CopyPayloadInputsLocally(mp, modelOutputDestination);    
         //compute passing in the event config portion of the model payload
-        String hmsFile = modelOutputDestination + payload.ModelName() + ".hms";
+        String hmsFile = modelOutputDestination + mp.getModel().getName() + ".hms";
         System.out.println("preparing to run " + hmsFile);
         Project project = Project.open(hmsFile);
-        project.computeRun(payload.ModelAlternative());
+        project.computeRun(mp.getModel().getAlternative());
         System.out.println("run completed for " + hmsFile);
         //push results to s3.
-        for (LinkedDataDescription output : payload.Outputs()) {
-            loader.UploadToS3(output.getResourceInfo().getAuthority(), output.getResourceInfo().getFragment(),modelOutputDestination + output.getName());
+        for (ResourcedFileData output : mp.getOutputs()) {
+            //ResourceInfo ri = new ResourceInfo();
+            //need to set the resource info
+            //Utilities.DownloadObject(info)  
+            Path path = Paths.get(modelOutputDestination + output.getFileName());
+            byte[] data;
+            try {
+                data = Files.readAllBytes(path);
+                Utilities.UploadFile(output.getResourceInfo(), data);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }  
         }
         Hms.shutdownEngine();
-        */
-
     }
 }

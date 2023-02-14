57bff1a98392144b81f060c18ffaab35648aadb6
Will Lehman <william.p.lehman@usace.army.mil>
2023-02-14 12:49:26 -0500

    updating to the latest java sdk

--- src/main/java/usace/wat/plugin/hmsrunner/hmsrunner.java
+++ src/main/java/usace/wat/plugin/hmsrunner/hmsrunner.java
@@ -1,6 +1,9 @@
 package usace.wat.plugin.hmsrunner;
 
 import usace.wat.plugin.*;
+
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -21,51 +24,47 @@ public class hmsrunner  {
     public static void main(String[] args) {
         System.out.println(PluginName + " says hello.");
         //check the args are greater than 1
-        Utilities.InitalizeFromEnv();
-
-        if(args.length!=2){
-            for(String s : args){
-                System.out.println("arg " + s);
-            }
-            System.out.println("Did not detect only payload `pathtopayload` argument");
-            return;
-        }else{
-            for(String s : args){
-                System.out.println("arg " + s);
-            }
-        }
-        //first arg should be a modelpayload check to see it is
-        String filepath = args[1];
+        PluginManager pm = new PluginManager();
         //load payload. 
-        ModelPayload mp = Utilities.LoadPayload(filepath);
+        Payload mp = pm.getPayload();
+        //get Alternative name
+        String model_name = (String) mp.getAttributes().get("model_name");
+        //get simulation name?
+        String alt_name = (String) mp.getAttributes().get("model_alternative");
         //copy the model to local if not local
         //hard coded outputdestination is fine in a container
-        String modelOutputDestination = "/model/"+mp.getModel().getName()+"/";
+        String modelOutputDestination = "/model/"+model_name+"/";
+        
         //download the payload to list all input files
-        Utilities.CopyPayloadInputsLocally(mp, modelOutputDestination);
-        for(ResourcedFileData i : mp.getInputs()){
-            if (i.getFileName().contains(mp.getModel().getName() + ".hms")){
+        String hmsFilePath = "";
+        
+        for(DataSource i : mp.getInputs()){
+            if (i.getName().contains(".hms")){
                 //compute passing in the event config portion of the model payload
-                String hmsFile = modelOutputDestination + i.getFileName();
-                System.out.println("preparing to run " + hmsFile);
-                Project project = Project.open(hmsFile);
-                project.computeRun(mp.getModel().getAlternative());
-                System.out.println("run completed for " + hmsFile);
+                hmsFilePath = modelOutputDestination + i.getName();
                 break;
             }
-        }
-        //walk("/model/");
-        //walk("/model/data/");    
+            byte[] bytes = pm.getFile(i, 0);
+            //write bytes locally.
+            File f = new File(modelOutputDestination, i.getName());
+            try(FileOutputStream outputStream = new FileOutputStream(f)){
+                outputStream.write(bytes);
+            }catch(Exception e){
+                e.printStackTrace();
+                return;
+            }
+        }    
+        System.out.println("preparing to run " + hmsFilePath);
+        Project project = Project.open(hmsFilePath);
+        project.computeRun(alt_name);
+        System.out.println("run completed for " + hmsFilePath);
         //push results to s3.
-        for (ResourcedFileData output : mp.getOutputs()) {
-            //ResourceInfo ri = new ResourceInfo();
-            //need to set the resource info
-            //Utilities.DownloadObject(info)  
-            Path path = Paths.get(modelOutputDestination + output.getFileName());
+        for (DataSource output : mp.getOutputs()) { 
+            Path path = Paths.get(modelOutputDestination + output.getName());
             byte[] data;
             try {
                 data = Files.readAllBytes(path);
-                Utilities.UploadFile(output.getResourceInfo(), data);
+                pm.putFile(data, output,0);
             } catch (IOException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
@@ -73,15 +72,5 @@ public class hmsrunner  {
         }
         Hms.shutdownEngine();
     }
-    /*private static void walk(String dir){
-        try (Stream<Path> walk = Files.walk(Paths.get(dir))) {
-            // We want to find only regular files
-            List<String> result = walk.filter(Files::isRegularFile)
-                    .map(x -> x.toString()).collect(Collectors.toList());
 
-            result.forEach(System.out::println);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }*/
 }

2088fc0b84e74b3b8b9f48c70311048235564736
Will Lehman <william.p.lehman@usace.army.mil>
2023-06-29 10:14:46 -0400

    adding in multiplier capabilities

--- src/main/java/usace/cc/plugin/hmsrunner/dsstoCsvAction.java
+++ src/main/java/usace/cc/plugin/hmsrunner/dsstoCsvAction.java
@@ -4,6 +4,7 @@ import hec.heclib.dss.HecTimeSeries;
 import hec.io.TimeSeriesContainer;
 import usace.cc.plugin.Action;
 import usace.cc.plugin.DataSource;
+import usace.cc.plugin.Payload;
 import usace.cc.plugin.PluginManager;
 
 public class dsstoCsvAction {
@@ -28,9 +29,16 @@ public class dsstoCsvAction {
         }
         //find destination parameter
         DataSource destination = action.getParameters().get("destination");
+        Payload payload = pm.getPayload();
         //read time series from source
         int pathIndex = 0;
         for(String p : source.getDataPaths()){//assumes datapaths for source and dest are ordered the same.
+            boolean hasMultiplier = payload.getAttributes().containsKey(p + "- multiplier");
+            float multiplier = 1.0f;
+            if (hasMultiplier){
+                float mult = Float.parseFloat((String) payload.getAttributes().get(p + " - multiplier"));
+                multiplier = mult;
+            }
             StringBuilder flows = new StringBuilder();
             TimeSeriesContainer tsc = new TimeSeriesContainer();
                 tsc.fullName = p;
@@ -47,6 +55,7 @@ public class dsstoCsvAction {
                 double delta = 1.0/24.0;//test with other datasets - probably need to make it dependent on d part.
                 double timestep = 0;
                 for(double f : values){
+                    f = f*multiplier;
                     flows = flows.append(timestep)
                                 .append(",")
                                 .append(f)
--- src/main/java/usace/cc/plugin/hmsrunner/dsstoHdfAction.java
+++ src/main/java/usace/cc/plugin/hmsrunner/dsstoHdfAction.java
@@ -6,6 +6,8 @@ import hec.heclib.dss.HecTimeSeries;
 import hec.io.TimeSeriesContainer;
 import usace.cc.plugin.Action;
 import usace.cc.plugin.DataSource;
+import usace.cc.plugin.Payload;
+import usace.cc.plugin.PluginManager;
 
 public class dsstoHdfAction {
     private Action action;
@@ -13,6 +15,8 @@ public class dsstoHdfAction {
         action = a;
     }
     public void computeAction(){
+        PluginManager pm = PluginManager.getInstance();
+        Payload payload = pm.getPayload();
         //find source 
         DataSource source = action.getParameters().get("source");
         //create dss reader
@@ -38,34 +42,41 @@ public class dsstoHdfAction {
         //read time series from source
         int datasetPathIndex = 0;
         for(String p : source.getDataPaths()){//assumes datapaths for source and dest are ordered the same.
-                TimeSeriesContainer tsc = new TimeSeriesContainer();
-                tsc.fullName = p;
+            boolean hasMultiplier = payload.getAttributes().containsKey(p + "- multiplier");
+            float multiplier = 1.0f;
+            if (hasMultiplier){
+                float mult = Float.parseFloat((String) payload.getAttributes().get(p + " - multiplier"));
+                multiplier = mult;
+            }
+            TimeSeriesContainer tsc = new TimeSeriesContainer();
+            tsc.fullName = p;
 
-                status = reader.read(tsc,true);
-                if (status <0){
-                    //panic?
-                    DSSErrorMessage error = reader.getLastError();
-                    error.printMessage();
-                    break;
-                }
-                double[] values = tsc.values;
-                double[] times = new double[values.length];
-                double delta = 1.0/24.0;//test with other datasets - probably need to make it dependent on d part.
-                double timestep = 0;
-                int i = 0;
-                for(double f : values){
-                    times[i] = timestep;
-                    timestep += delta;
-                    i++;
-                }
-                //write time series to destination
-                try {
-                    writer.write(values,times,destination.getDataPaths()[datasetPathIndex]);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    return;
-                }
-                datasetPathIndex++;
+            status = reader.read(tsc,true);
+            if (status <0){
+                //panic?
+                DSSErrorMessage error = reader.getLastError();
+                error.printMessage();
+                break;
+            }
+            double[] values = tsc.values;
+            double[] times = new double[values.length];
+            double delta = 1.0/24.0;//test with other datasets - probably need to make it dependent on d part.
+            double timestep = 0;
+            int i = 0;
+            for(double f : values){
+                values[i] = f*multiplier;
+                times[i] = timestep;
+                timestep += delta;
+                i++;
+            }
+            //write time series to destination
+            try {
+                writer.write(values,times,destination.getDataPaths()[datasetPathIndex]);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return;
+            }
+            datasetPathIndex++;
                                         
         }
         //close reader

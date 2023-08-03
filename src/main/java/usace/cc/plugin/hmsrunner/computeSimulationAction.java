52d70d188e435be78c6a5b440a1e92ea4ba761c3
Andrew Song <andrewsong.j@gmail.com>
2023-08-03 13:30:45 -0400

    detect and print success/error in simulation run

--- src/main/java/usace/cc/plugin/hmsrunner/computeSimulationAction.java
+++ src/main/java/usace/cc/plugin/hmsrunner/computeSimulationAction.java
@@ -3,6 +3,10 @@ package usace.cc.plugin.hmsrunner;
 import hms.model.Project;
 import usace.cc.plugin.Action;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class computeSimulationAction {
     private Action action;
     private String simulationName;
@@ -13,10 +17,37 @@ public class computeSimulationAction {
     public void computeAction(){
         String hmsFilePath = action.getParameters().get("project_file").getPaths()[0];
         System.out.println("opening project " + hmsFilePath);
-        Project project = Project.open(hmsFilePath);
+        Project project;
+        try {
+            project = Project.open(hmsFilePath);
+        } catch (Exception e) {
+            System.out.println("ERROR failed to open the " + hmsFilePath + " project.");
+            return;
+        }
         System.out.println("preparing to run Simulation " + simulationName);
         project.computeRun(simulationName);
-        System.out.println("Simulation run completed for " + hmsFilePath);
+
+        int lastDotIndex = hmsFilePath.lastIndexOf('.'); // Get index of the dot before hms, e.g model.hms
+        String logFilePath = hmsFilePath.substring(0, lastDotIndex) + ".log";
+        boolean simError = containsError(logFilePath);
+        if(simError)
+            System.out.println("ERROR running simulation for " + hmsFilePath);
+        else
+            System.out.println("SUCCESS running simulation for " + hmsFilePath);
         project.close();
+    }
+
+    private boolean containsError(String filepath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filepath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.startsWith("ERROR")) {
+                    return true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }

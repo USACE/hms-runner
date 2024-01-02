225b9412fa83f5d342818fbd258c9e73750a10f1
Will Lehman <william.p.lehman@usace.army.mil>
2024-01-02 08:46:40 -0500

    removing multiplier

--- src/main/java/usace/cc/plugin/hmsrunner/DssToHdfObservedFlowsAction.java
+++ src/main/java/usace/cc/plugin/hmsrunner/DssToHdfObservedFlowsAction.java
@@ -1,15 +1,11 @@
 package usace.cc.plugin.hmsrunner;
 
-import hdf.hdf5lib.H5;
-import hdf.hdf5lib.HDF5Constants;
 import hdf.hdf5lib.exceptions.HDF5LibraryException;
 import hec.heclib.dss.DSSErrorMessage;
 import hec.heclib.dss.HecTimeSeries;
 import hec.io.TimeSeriesContainer;
 import usace.cc.plugin.Action;
 import usace.cc.plugin.DataSource;
-import usace.cc.plugin.Payload;
-import usace.cc.plugin.PluginManager;
 
 public class DssToHdfObservedFlowsAction {
     private Action action;
@@ -17,8 +13,6 @@ public class DssToHdfObservedFlowsAction {
         action = a;
     }
     public void computeAction(){
-        PluginManager pm = PluginManager.getInstance();
-        Payload payload = pm.getPayload();
         //find source 
         DataSource source = action.getParameters().get("source");
         //create dss reader
@@ -44,12 +38,6 @@ public class DssToHdfObservedFlowsAction {
         //read time series from source
         int datasetPathIndex = 0;
         for(String p : source.getDataPaths()){//assumes datapaths for source and dest are ordered the same.
-            boolean hasMultiplier = payload.getAttributes().containsKey(p + "- multiplier");
-            float multiplier = 1.0f;
-            if (hasMultiplier){
-                float mult = Float.parseFloat((String) payload.getAttributes().get(p + " - multiplier"));
-                multiplier = mult;
-            }
             TimeSeriesContainer tsc = new TimeSeriesContainer();
             tsc.fullName = p;
 
@@ -61,11 +49,6 @@ public class DssToHdfObservedFlowsAction {
                 break;
             }
             double[] values = tsc.values;
-            int i = 0;
-            for(double f : values){
-                values[i] = f*multiplier;
-                i++;
-            }
             //write time series to destination
             try {
                 writer.writeResSimReleases(values,destination.getDataPaths()[datasetPathIndex]);

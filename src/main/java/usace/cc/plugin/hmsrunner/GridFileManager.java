e9b74b647d60690a44718ce76825b986fa3112cb
Will Lehman <william.p.lehman@usace.army.mil>
2025-04-25 14:40:08 -0400

    improving logic to more reliably detect conditions.

--- src/main/java/usace/cc/plugin/hmsrunner/GridFileManager.java
+++ src/main/java/usace/cc/plugin/hmsrunner/GridFileManager.java
@@ -2,6 +2,8 @@ package usace.cc.plugin.hmsrunner;
 
 import java.util.ArrayList;
 
+import org.python.antlr.PythonParser.continue_stmt_return;
+
 public class GridFileManager {
 //read in a grid file - parse it into the precipitation and temperature grids
 //provide ability to export a grid file (as bytes or a bytestream) with a single precip and temperature grid
@@ -14,23 +16,30 @@ public GridFileManager(String[] lines){
     ArrayList<Block> blocks = new ArrayList<Block>();
     ArrayList<Block> precip = new ArrayList<Block>();
     ArrayList<Block> temp = new ArrayList<Block>();
+    Boolean isTemp = false;
+    Boolean isPrecip = false;
     for(String line :lines){
         blines.add(line);
+        if (line.substring(0,10).contains("Grid Type: ")){
+            if (line.contains("Precipitation")){
+                isPrecip = true;
+            }else if(line.contains("Temperature")){
+                isTemp = true;
+            }
+        }
         if( line.equals("END:")){
             String[] ablines = (String[])blines.toArray();
             Block b = new Block(ablines);
-            if (ablines.length>2){
-                if(ablines[1].contains("Precipitation")){//what if there are newlines? i think this could be 1 or 2
+                if(isPrecip){
                     precip.add(b);
-                }else if(ablines[1].contains("Temperature")){
+                }else if(isTemp){
                     temp.add(b);
                 }else{
                     blocks.add(b);
-                }
-            }else{
-                blocks.add(b);
-            }            
+                }         
             blines = new ArrayList<String>();
+            isTemp = false;
+            isPrecip = false;
         }
     }
 }
@@ -42,14 +51,32 @@ public String[] Write(String gridName) {
         }
     }
     for(Block b : PrecipGrids){
-        if(b.Lines[0].contains(gridName)){//what if it is a new line?
+        boolean isRightGrid = false;
+        for(String l: b.Lines){
+            if (l.substring(0,5).equals("Grid: ")){
+                if (l.contains(gridName)){
+                    isRightGrid = true;
+                    break;
+                }
+            }
+        }
+        if(isRightGrid){
             for(String l : b.Lines){
                 lines.add(l);
             }
         }
     }
     for(Block b : TemperatureGrids){
-        if(b.Lines[0].contains(gridName)){//what if it is a new line?
+        boolean isRightGrid = false;
+        for(String l: b.Lines){
+            if (l.substring(0,5).equals("Grid: ")){
+                if (l.contains(gridName)){
+                    isRightGrid = true;
+                    break;
+                }
+            }
+        }
+        if(isRightGrid){
             for(String l : b.Lines){
                 lines.add(l);
             }
